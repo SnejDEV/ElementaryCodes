bd41022c6c37efa0b95d60a8a56b78497dd51653
piNg <66051560+snejDev@users.noreply.github.com>
2020-06-13 04:53:32 +0530

    Create pivotPush.py

---
+++ DataStructures/Algorithms/Concepts/RecursiveCodes/pivotPush.py
@@ -0,0 +1,24 @@
+#This the concept used in Quick Sort
+def swap(n,k,a):
+    a[n]=a[n]+a[k]
+    a[k]=a[n]-a[k]
+    a[n]=a[n]-a[k]
+    return(a)
+
+def lpivot(a):
+    l = len(a)-1
+    p=len(a)-1
+    x=0
+    while(x!=p):
+        print(a)
+        if(a[p]<a[x]):
+            if((p-x)==1):
+                a=swap(x,p,a)
+                p=p-1
+                continue
+            a=swap(x,p-1,a)
+            a=swap(p,p-1,a)
+            p=p-1
+            continue
+        x+=1
+    return(a)
