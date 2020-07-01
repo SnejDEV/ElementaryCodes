bd7a50b035e618a820c741c72978d485bb4618b1
piNg <66051560+snejDev@users.noreply.github.com>
2020-07-01 15:41:52 +0530

    Update CreateGraphs.py

--- DataStructures/Graphs/CreateGraphs.py
+++ DataStructures/Graphs/CreateGraphs.py
@@ -1,4 +1,4 @@
-class Graph:
+'''class Graph:
      def __init__(self):
           self.graph = {}
           self.length = 0
@@ -10,4 +10,21 @@ class Graph:
      def addEdge(self,node1,node2):
           self.graph[node1].append(node2)
           self.graph[node2].append(node1)
-          
+'''
+
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.edges = []
+
+class Graph():
+    def __init__(self):
+        self.Graph = {}
+
+    def addNode(self,i,val):
+        self.Graph[i]=Node(val)
+
+    def addEdge(self,i1,i2,d):                              #d is direction, 1 is uni nd 2 is bi
+        self.Graph[i1].edges.append(self.Graph[i2])
+        if(d==2):
+            self.Graph[i2].edges.append(self.Graph[i1])
