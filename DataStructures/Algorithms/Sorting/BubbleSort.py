ed4bb6ac25eaf20bf0d40f29fbbda82fbbced8d3
piNg <66051560+snejDev@users.noreply.github.com>
2020-07-02 10:54:43 +0530

    Update BubbleSort.py

--- DataStructures/Algorithms/Sorting/BubbleSort.py
+++ DataStructures/Algorithms/Sorting/BubbleSort.py
@@ -1,8 +1,18 @@
-def sort(a):                                 #timeComplexity = O(n^2) and spaceComplexity = O(1)
+'''def sort(a):                                 #timeComplexity = O(n^2) and spaceComplexity = O(1)
      for x in range(0,len(a)-1):
           for y in range(0,(len(a)-1)-x):
                if(a[y]>a[y+1]):
                     a[y]=a[y]+a[y+1]
                     a[y+1]=a[y]-a[y+1]
                     a[y]=a[y]-a[y+1]
-     return(a) 
+     return(a) '''
+
+
+def BBS(a):
+    for x in range(1,len(a)):
+        for y in range(0,len(a)-x):
+            if(a[y]>a[y+1]):
+                a[y]+=a[y+1]
+                a[y+1]=a[y]-a[y+1]
+                a[y]=a[y]-a[y+1]
+    return(a)
